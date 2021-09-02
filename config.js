caf1c18dd3e704c909b10a2db8d58c1418b0ce74
Charles Hill <chill@degreesofzero.com>
2021-09-02 16:29:10 +0100

    Default host names should be localhost or 127.0.0.1

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 const crypto = require('crypto');
-const path = require('path');
 
 // Optionally define environment variables by placing a .env file in the root of the project directory.
 // For details:
@@ -8,7 +7,7 @@ require('dotenv').config();
 
 let config = {
 	lnurl: {
-		host: process.env.LNURL_TOOLBOX_HOST || '0.0.0.0',
+		host: process.env.LNURL_TOOLBOX_HOST || 'localhost',
 		port: parseInt(process.env.LNURL_TOOLBOX_PORT || 3000),
 		url: process.env.LNURL_TOOLBOX_URL || null,
 		endpoint: process.env.LNURL_TOOLBOX_ENDPOINT || '/u',
@@ -22,7 +21,7 @@ let config = {
 		},
 	},
 	web: {
-		host: process.env.LNURL_TOOLBOX_WEB_HOST || '0.0.0.0',
+		host: process.env.LNURL_TOOLBOX_WEB_HOST || 'localhost',
 		port: parseInt(process.env.LNURL_TOOLBOX_WEB_PORT || 8080),
 		url: process.env.LNURL_TOOLBOX_WEB_URL || null,
 		session: JSON.parse(process.env.LNURL_TOOLBOX_WEB_SESSION || '{"secret":"","resave":true,"saveUninitialized":false,"proxy":false,"cookie":{"httpOnly":true,"expires":false,"path":"/","sameSite":true,"secure":false}}'),
@@ -30,7 +29,7 @@ let config = {
 	uriSchemaPrefix: process.env.LNURL_TOOLBOX_URI_SCHEMA_PREFIX || 'LIGHTNING:',
 	mock: {
 		clightning: {
-			host: process.env.LNURL_TOOLBOX_MOCK_CLIGHTNING_HOST || '0.0.0.0',
+			host: process.env.LNURL_TOOLBOX_MOCK_CLIGHTNING_HOST || '127.0.0.1',
 			port: parseInt(process.env.LNURL_TOOLBOX_MOCK_CLIGHTNING_PORT || 9735),
 			hostname: process.env.LNURL_TOOLBOX_MOCK_CLIGHTNING_HOSTNAME || null,
 		},
